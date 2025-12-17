b90d25eba0e371c40c53b62ab138a497df9d07d4
يزيد مصطفى الجهني <yazeed@MacBook-Air-mac.local>
2025-12-17 08:44:55 +0300

    add: add function  that sort elemint in selection alograthim

--- sort.h
+++ sort.h
@@ -13,9 +13,9 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+const int n;
+struct listint_s *prev;
+struct listint_s *next;
 } listint_t;
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
