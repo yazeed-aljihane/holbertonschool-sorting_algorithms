08e4d3354c849bd5bb75a6d6430bc1c15ce9cc56
haitham71 <hri7isd@gmail.com>
2025-12-18 19:09:46 +0300

    fix-- betty

--- sort.h
+++ sort.h
@@ -1,9 +1,6 @@
 #ifndef SORT_H
 #define SORT_H
-
-
-
-
+#include <stddef.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -11,12 +8,11 @@
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-#include <stddef.h>
 typedef struct listint_s
 {
-const int n;
-struct listint_s *prev;
-struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
