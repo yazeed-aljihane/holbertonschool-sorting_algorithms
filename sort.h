730f6dee0b00ee5b8facf780fd8c0429e04cf9a4
haitham71 <hri7isd@gmail.com>
2025-12-18 19:04:01 +0300

    add-- Prototype

--- sort.h
+++ sort.h
@@ -21,4 +21,5 @@ struct listint_s *next;
 void selection_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
 #endif /*SORT_H*/
